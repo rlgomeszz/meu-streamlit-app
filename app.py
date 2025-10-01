4d4713cee2f18227bd8a7743fcdac4b1d1f1cdad
rlgomeszz <ricardojefferson941@gmail.com>
2025-10-01 08:58:10 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Meu Projeto", layout="centered")
+
+st.title("Meu Projeto Streamlit")
+# Nome logo abaixo do título
+st.markdown("Por: Jefferson Ricardo Gomes de Souza")
+
+st.write("Bem-vindo ao meu app! Aqui vai o restante do conteúdo...")
